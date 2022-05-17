4b6437cae4eb676adb5c5fcef55f5b03530325bb
ESDUNCAN <d.eric@rogers.com>
2022-05-17 18:01:01 +0000

    installed nodemon

--- express_server.js
+++ express_server.js
@@ -1,17 +1,49 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+function generateRandomString(length) {
+  let result = ""
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log("We are finally in the Post request on the server", req.body);  // Log the POST request body to the console
+  // get the longURL from the form request
+  const newLongURL = req.body["longURL"]
+
+  // create short URL so it can be stored in database
+  const newshortURL = generateRandomString(6)
+  // store short + long in database
+
+  urlDatabase[newshortURL] = newLongURL
+  res.redirect(`/urls/${newshortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL])
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
